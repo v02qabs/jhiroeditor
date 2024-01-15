d9be482ce6698ab1b79c4d72fd3425de39d9c17c
v02qabs <takesue090@gmail.com>
2024-01-15 22:12:35 +0900

    editor-MenuItem-add

--- editor/editor.java
+++ editor/editor.java
@@ -27,7 +27,7 @@ class editor
 }
 
 
-class winpanel extends JFrame implements ActionListener
+class winpanel extends JFrame
 {
 	String fname;
 	String filename;
@@ -89,44 +89,72 @@ class winpanel extends JFrame implements ActionListener
 	private void  past()
 	{
 		edit = new JTextArea();
+		edit.setFont(new Font("Arial", Font.PLAIN, 16));
 		  JScrollPane scrollpane = new JScrollPane(edit);
     scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
     scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-		save = new JButton("save");
-		save.addActionListener(this);
-		new_files_button = new JButton("new");
-		new_files_button.addActionListener(this);
-		open_file_button = new JButton("open");
-		open_file_button.addActionListener(this);
-		JPanel all_panel = new JPanel();	
-		JPanel control_panel = new JPanel();
-		GridLayout control = new GridLayout(1,3);
-		GridLayout layout = new GridLayout(2,2);
-		control_panel.setLayout(control);
-		all_panel.add(control_panel);
-		control_panel.add(save);
-		control_panel.add(new_files_button);
-		control_panel.add(open_file_button);
+		JMenuBar menubar = new JMenuBar();		
+		JMenu file_menu = new JMenu("ファイル")	;
+		menubar.add(file_menu);
+		JMenuItem open_file_button = new JMenuItem("開く");
+		file_menu.add(open_file_button);
+		open_file_button.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					System.out.println("開く");
+					open();
+				}
+		});
+		JMenuItem save = new JMenuItem("保存");
+		file_menu.add(save);
+		save.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					save();
+				}
+		});
 		
+		JMenuItem override_menu = new JMenuItem("上書き保存");
+		file_menu.add(override_menu);
+		override_menu.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					System.out.println("上書き保存");
+				}
+		});
+		JMenuItem new_file_button = new JMenuItem("新規");
+		file_menu.add(new_file_button);
+		new_file_button.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					System.out.println("新規");
+					new_file();
+				}
+		});
+
+		JPanel all_panel = new JPanel();
+		setJMenuBar(menubar);
+			
+		JPanel control_panel = new JPanel();
+		GridLayout control = new GridLayout(3,1);
+		GridLayout layout = new GridLayout(1,2);
+		control_panel.setLayout(control);
 		all_panel.setLayout(layout);
 		
-		all_panel.add(control_panel);
 		all_panel.add(scrollpane);
 		Container  contents = getContentPane();
 		contents.add(all_panel);
 	}
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		System.out.println("typed subject.");
-		if(e.getSource() == save)
-		{
-			save();
-		}else if(e.getSource() == new_files_button){
-			new_file();
-		}	
-		else if(e.getSource() == open_file_button){
+	private void open(){
 			System.out.println("open");
 			String os;
 			try{
@@ -143,41 +171,39 @@ class winpanel extends JFrame implements ActionListener
 			{
 				System.out.println("Runtime error");
 			}
-		}
-		else{
-			System.out.println("error");
-		}
 		
-		
-
 	}
+	
 	private void new_file(){
-	System.out.println("新規作成");
-	setTitle("新規");
-	set_filename(null);
-	//edit = new JTextArea();
-	edit.setText("");
+		System.out.println("新規作成");
+		setTitle("新規");
+		set_filename(null);
+		//edit = new JTextArea();
+		edit.setText("");
 	}
 	
 	private void save()
 	{
-	try{	
-		System.out.println("save");
-		String sub = edit.getText().toString();
-		System.out.println("g : " + sub);
-		File save_file = new File(get_filename());
-		BufferedWriter writer = new BufferedWriter(new FileWriter(save_file));
-		writer.write(sub);
-		writer.close();
+		try{	
+			System.out.println("save");
+			String sub = edit.getText().toString();
+			System.out.println("g : " + sub);
+			File save_file = new File(get_filename());
+			BufferedWriter writer = new BufferedWriter(new FileWriter(save_file));
+			writer.write(sub);
+			writer.close();
+		}
+		catch(Exception error)
+		{
+			error();
+		}
 	}
-	catch(Exception error)
-	{
-	error();
-	}
-	}
+
 	private void error()
 	{
 		System.out.println("error");
 	}
+	
 }
+
 
