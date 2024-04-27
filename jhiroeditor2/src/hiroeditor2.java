713392a30092800f0b2696e22c4fcb77243aea10
v02qabs <takesue090@gmail.com>
2024-04-27 11:37:40 +0900

    jhiroeditor2-main_panel

---
+++ jhiroeditor2/src/hiroeditor2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class hiroeditor{
+	public hiroeditor(){}
+	public static void main(String[] args){
+		System.out.println(args[0]);
+		String fname = args[0];
+		new hiroeditor().init(fname);
+	}
+	public void init(String fname){
+		new Start_Editor().start_editor(fname);
+
+	}
+}
