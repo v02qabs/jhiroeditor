c2ebd194c27c6ac8f6496026d69d47d09c941ae5
v02qabs <takesue090@gmail.com>
2024-05-01 09:32:56 +0900

    jftp-java-package-org.apache

---
+++ Jftp/comp.sh
@@ -0,0 +1,4 @@
+javac -cp /usr/share/java/commons-net.jar: ./jftp.java -d ./bin/.
+cd bin
+
+java -cp /usr/share/java/commons-net.jar: jftp hello
--- Jftp/jftp.java
+++ Jftp/jftp.java
@@ -1,10 +1,47 @@
 import java.io.*;
+import org.apache.commons.net.ftp.*;
 
 class jftp
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello jftp.");
+		try{
+			String args0 = args[0];
+			System.out.println("args0: " + args0);
+			try{
+				String args1 = args[1];
+				new jftp().command(args0, args1);
+			}
+			catch(Exception error){
+				System.out.println("args1 = null");
+				new jftp().command(args0, null);
+
+			}
+
+		}
+		catch(Exception error){
+			String args1 = null;
+			System.out.println("args was null");
+		}
+
 	}
+	private void command(String args0, String args1){
+		System.out.println(args0 + " , " + args1);
+		if(args0.equals("hello") || args1.equals(null))
+		{
+					System.out.println("Hello Worlds.");
+		}
+		else if(args0.equals("add") || args1.equals(".")){
+			System.out.println("add .");
+		}
+		else if(args0.equals("push") || args1.equals(null))
+		{
+			System.out.println("push");
+		}
+
+
+	}
+
 }
 
